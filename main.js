531993151d30d75fcdaf1c51a268a405047cd8fc
deyaa1997 <deyah.mosa@gmail.com>
2021-04-23 17:50:29 +0300

    hide the string and add add the alpabets

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
 const start = () => {
-    let str = "play"
-    const body = document.querySelector("body")
-    // remove the last body inner
-    body.innerHTML = "";
-    let x =7
-    // the counter for chances
-    const counter = document.createElement("h1")
-    counter.id= "h";
-    counter.innerHTML=x;
-    body.append(counter)
+  let str = "play";
+  const body = document.querySelector("body");
+  // remove the last body inner
+  body.innerHTML = "";
+  let x = 7;
+  // the counter for chances
+  const counter = document.createElement("h1");
+  counter.id = "h";
+  counter.innerHTML = x;
+  body.append(counter);
 
-    const div = document.createElement("div")
-    div.id= "h"
-    body.append(div)
+  const div = document.createElement("div");
+  div.id = "div";
+  body.append(div);
 
-    const word = document.createElement("p")
-    word.id = "word"
+  const divWord = document.createElement("div");
+  divWord.id = "divWord";
+  div.append(divWord);
 
-    let strNew = ""
-    for (let x =0 ; x <str.length ;x++){
-        strNew += str[x] + " " ;
-    }
-    
-    word.innerHTML=strNew;
-    div.append(word);
+  for (let x = 0; x < str.length; x++) {
+    const alph = document.createElement("p");
+    alph.id = "alph" + x;
+    alph.innerHTML = str[x];
+    divWord.append(alph);
+  }
 
+  const divSpace = document.createElement("div");
+  divSpace.id = "divSpace";
+  div.append(divSpace);
 
-    let _word = ""
-    for (let x =0 ; x <str.length ;x++){
-        _word += "_ ";
-    }
+  for (let x = 0; x < str.length; x++) {
+    const Space = document.createElement("p");
+    Space.id = "Space";
+    Space.innerHTML = "_";
+    divSpace.append(Space);
+  }
 
-    const underword = document.createElement("p")
-    underword.id = "word"
-    underword.innerHTML = _word
-    div.append(underword);
-}
+  const divChar = document.createElement("div")
+  divChar.id = "divChar"
+  div.append(divChar);
+  
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let z = 0 ; z < alphabet.length ; z++ ){
+      const char = document.createElement("button")
+      char.id = "char" + z ;
+      char.innerHTML = alphabet[z]
+      divChar.append(char);
+  }
+};
