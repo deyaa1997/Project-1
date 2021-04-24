815678c3a863f3a17d97b678f26e06b6c46120ae
deyaa1997 <deyah.mosa@gmail.com>
2021-04-25 02:57:48 +0300

    Add Array of random Words and the program work correctly

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+const arr = ["abruptly" , "absurd" , "abyss" , "affix" , "askew" , "avenue" ,
+"awkward" , "axiom" , "azure" , "bagpipes" , "bandwagon" , "banjo" , "bayou","beekeeper"]
+
+let rand = arr[Math.floor(Math.random() * arr.length)]
 const start = () => {
-  let str = "danger";
+  let str = rand;
   const body = document.querySelector("body");
   // remove the last body inner
   body.innerHTML = "";
   body.style.backgroundColor = "rgb(169,91,44)"
   let c = 7;
   // the counter for chances
-  const counter = document.createElement("h1");
-  counter.id = "counter";
-  counter.innerHTML = c;
-  body.append(counter);
+  
+
 
   const div = document.createElement("div");
   div.id = "div";
   body.append(div);
+
+  const div1 = document.createElement("div")
+  div1.id="div1"
+  div.append(div1)
+
+  const counter = document.createElement("h1");
+  counter.id = "counter";
+  counter.innerHTML = "You Have   " + c + "  Attempts";
+  div1.append(counter);
+
+  const img1 = document.createElement("img")
+  img1.setAttribute("src", "pic/7.png");
+  img1.id= "img"
+  div1.appendChild(img1)
 
   const divWord = document.createElement("div");
   divWord.id = "divWord";
@@ -73,15 +89,45 @@ const start = () => {
       document.getElementById("char" + z).innerHTML = "";
       document.getElementById("char" + z).style.backgroundColor =
         "darkgoldenrod";
-      document.getElementById("counter").innerHTML = --c;
+      document.getElementById("counter").innerHTML = "You Have   " + --c + "  Attempts";
+      img1.innerHTML = ""
+      img1.id = "img"
       if (sum >= 1) {
-        document.getElementById("counter").innerHTML = ++c;
+        document.getElementById("counter").innerHTML = "You Have   " + ++c + "  Attempts";
       }
+      img1.setAttribute("src" , "pic/" + c + ".png")
       if (s === str.length){
-        alert ("you win")
+        img1.innerHTML = ""
+        img1.id = "img"
+        img1.setAttribute("src" , "win.png")
+        divChar.innerHTML= ""
+        const playAgain = document.createElement("img")
+        playAgain.id = "img"
+        playAgain.setAttribute("src" , "try.jpg")
+        divChar.appendChild(playAgain)
+        const but = document.createElement("button")
+        but.id = "1"
+        divChar.appendChild(but)
+        document.getElementById("1").addEventListener("click",function(){
+          return start()
+        })
       }
-      if (document.getElementById("counter").innerHTML === "0" ){
-        alert("Game Over")
+
+      if (document.getElementById("counter").innerHTML === "You Have   " + 0 + "  Attempts" ){
+        img1.innerHTML = ""
+        img1.id = "img"
+        img1.setAttribute("src" , "pic/loose2.jpg")
+        divChar.innerHTML = ""
+        const playAgain = document.createElement("img")
+        playAgain.id = "img"
+        playAgain.setAttribute("src" , "try.jpg")
+        divChar.appendChild(playAgain)
+        const but = document.createElement("button")
+        but.id = "1"
+        divChar.appendChild(but)
+        document.getElementById("1").addEventListener("click",function(){
+          return start()
+        })
       }
     });
   }
